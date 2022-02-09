6b81f4cd96346c565423f0cced47196030abe820
Raziqkhan07 <98086591+Raziqkhan07@users.noreply.github.com>
2022-02-09 13:07:04 +0530

    Add files via upload

--- Class_and_Instance_Variables.py
+++ Class_and_Instance_Variables.py
@@ -1,16 +1,21 @@
+# Python3 program to show that the variables with a value
+# assigned in the class declaration, are class variables and
+# variables inside methods and constructors are instance
+# variables.
+
+# Class for Dog
 class Dog:
     # Class Variable
     animal = 'dog'
 
     # The init method or constructor
-    def __init__(self):(self, breed, color):
+    def __init__(self, breed, color):
         # Instance Variable
         self.breed = breed
         self.color = color
 
-    # Objects of Dog class
 
-
+# Objects of Dog class
 Rodger = Dog("Pug", "brown")
 Buzo = Dog("Bulldog", "black")
 
@@ -26,6 +31,5 @@ print('Color: ', Buzo.color)
 
 # Class variables can be accessed using class
 # name also
-
 print("\nAccessing class variable using class name")
 print(Dog.animal)
